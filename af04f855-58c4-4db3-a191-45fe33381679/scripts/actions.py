8cdd20e38e310a0760c4a727fe5d72c5ba0a64d8
Froh <Frohlemageblanc@users.noreply.github.com>
2018-03-21 01:12:44 +0100

    Mostly typo and optimizing stuff

--- af04f855-58c4-4db3-a191-45fe33381679/scripts/actions.py
+++ af04f855-58c4-4db3-a191-45fe33381679/scripts/actions.py
@@ -199,16 +199,7 @@ def createUnit(group, x = 0, y = 0):
 	choiceList = ['Tyranid', 'Astra Militarum', 'Chaos', 'Dark Eldar', 'Eldar', 'Space Marine', 'Ork', 'Tau', 'Necron']
 	choice = askChoice("For which Faction do you want to create a Unit ?", choiceList)
 	if choice == 0 : return
-	elif choice == 1 : choiceArmy = 'Tyranid'
-	elif choice == 2 : choiceArmy = 'Astra Militarum'
-	elif choice == 3 : choiceArmy = 'Chaos'
-	elif choice == 4 : choiceArmy = 'Dark Eldar'
-	elif choice == 5 : choiceArmy = 'Eldar'
-	elif choice == 6 : choiceArmy = 'Space Marine'
-	elif choice == 7 : choiceArmy = 'Ork'
-	elif choice == 8 : choiceArmy = 'Tau'
-	else : choiceArmy = 'Necron'
-	guid,quantity=askCard({'Type':'Army', 'Faction':choiceArmy })
+	guid,quantity=askCard({'Type':'Army', 'Faction': choiceList[choice - 1] })
 	if guid == None: return
 	if me.isInverted: 
 		X1=-150
@@ -765,28 +756,30 @@ def moveOneRandom(group):
 def CheckTournamentValid(group, x=0, y=0):
 	LegacyLegal=["01-Core Set", "02-Warlord Cycle", "03-The Great Devourer", "04-Planetfall Cycle" "05-Legions of Death", "06-Deathworld Cycle"]
 	ApokaLegal=["01-Core Set", "02-Warlord Cycle", "03-The Great Devourer", "04-Planetfall Cycle" "05-Legions of Death", "06-Deathworld Cycle", "APK-01-Navida Prime Cycle","APK-02-Defenders of the Faith","APK-03-Confrontation Cycle"]
-	choiceList = ['Legacy (FFG)', 'Extended (FFG + Apoka)']
-	choice = askChoice("Which Format do you want to check ?", choiceList)
+	choiceList = ['FFG', 'FFG + Apoka']
+	choice = askChoice("Which format do you want to check ?", choiceList)
 	valid = True
 	for card in me.deck:
-		if (choice == 1):
-			if (card.set not in LegacyLegal):
-				whisper("This card {} is not tournament legal : Set= {}".format(card.Name, card.set))
+		if choice == 0:
+			return
+		elif choice == 1:
+			if card.set not in LegacyLegal:
+				whisper("The card {} is not tournament legal : Set= {}".format(card.Name, card.set))
 				valid = False
 				break
-		elif (choice == 2):
-			if (card.set not in ApokaLegal):
-				whisper("This card {} is not tournament legal : Set= {}".format(card.Name, card.set))
+		elif choice == 2:
+			if card.set not in ApokaLegal:
+				whisper("The card {} is not tournament legal : Set= {}".format(card.Name, card.set))
 				valid = False
 				break
 		else:
-			whisper("This card {} is not tournament legal : Set= {}".format(card.Name, card.set))
+			whisper("The card {} is not tournament legal : Set= {}".format(card.Name, card.set))
 			valid = False
 			break
 	if valid == True:
 		if choice == 1:
-			notify("{}'s deck contains only Legacy legal cards".format(me))
+			notify("{}'s deck is legal in a FFG tournament format".format(me))
 		elif choice == 2:
-			notify("{}'s deck contains only Extended legal cards".format(me))
+			notify("{}'s deck is legal in a FFG + Apoka format".format(me))
 	else:
 		notify("{}'s deck does contain some invalid cards for the selected format".format(me))
